c835434283d109127c3d2634f812fc842f570192
rayen <rayenbouhachem02@gmail.com>
2026-06-08 17:12:26 +0100

    quick change

--- miniml/optim/gradiant_descent.py
+++ miniml/optim/gradiant_descent.py
@@ -7,4 +7,14 @@ class GradientDescent:
         self.epochs = epochs
         self.verbose_every = verbose_every
 
+    def fit(self, X, y, model):
+        for epoch in range(self.epochs):
+            y_pred = model.forward(X)
+            loss = model.loss(y, y_pred)
+            model.backward(y, y_pred)
+            model.update(self.learning_rate)
+
+            if self.verbose_every is not None and epoch % self.verbose_every == 0:
+                print(f'Epoch {epoch}, Loss: {loss}')
+                
         
